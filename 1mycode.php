9fd4d7e9217bcf3cd4b4a0ef1277606f55ecabcd
sagarjangid41 <60397881+sagarjangid41@users.noreply.github.com>
2020-06-18 11:46:23 +0530

    Update 1mycode.php

--- 1mycode.php
+++ 1mycode.php
@@ -1,4 +1,7 @@
 <?php
 print "done" ;
+echo
+<html>
 <img src=" https://examplebuckettftest1ddd2233.s3.ap-south-1.amazonaws.com/Sws.jpg " width="280" height="125" title="Logo of a company" alt="Logo of a company" />
+</html>
 ?>
