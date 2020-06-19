1caaf93bed769d809375045495c4193a43ae90ad
sagarjangid41 <60397881+sagarjangid41@users.noreply.github.com>
2020-06-19 12:40:18 +0530

    Update 1mycode.php

--- 1mycode.php
+++ 1mycode.php
@@ -1,2 +1,2 @@
 Image
-<img src=" https://examplebuckettftest1ddd2233.s3.ap-south-1.amazonaws.com/Sws.jpg" wigth="200" height="200" />
+<img src=" https://terraformbucket12121212.s3.ap-south-1.amazonaws.com/image.jpg" wigth="200" height="200" />
